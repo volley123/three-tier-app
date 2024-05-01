b56525b47101b3c96afc7c27d8efb7b72fc7fe2b
Varshil Desai <varshil.desai@qloudx.com>
2024-05-01 17:06:09 +0530

    added latest changes in three-tier app

--- Application-Code/frontend/src/index.js
+++ Application-Code/frontend/src/index.js
@@ -1,5 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <React.StrictMode>
+        <div className="app-wrapper">
+            <App />
+        </div>
+    </React.StrictMode>,
+    document.getElementById("root")
+);
+
